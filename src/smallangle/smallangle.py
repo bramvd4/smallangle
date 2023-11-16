8335b41c226ce536c31211eb29045ef92964f841
bramvd4 <135610086+bramvd4@users.noreply.github.com>
2023-11-16 11:09:51 +0100

    added click commands for sin and tan

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,9 +1,22 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def smallangle():
+    pass
+
+
+@smallangle.command("sin")
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="number of steps between 0 and 2 pi",
+    show_default=True,
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
@@ -11,6 +24,14 @@ def sin(number):
     return
 
 
+@smallangle.command("tan")
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="number of steps between 0 and 2 pi",
+    show_default=True,
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -19,4 +40,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    smallangle()
